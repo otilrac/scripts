b4520588ed7ddd9cdc46208d4861b95d3b7c15e5
yakorev <yakorev_ivan@mail.ru>
2019-03-04 23:23:44 +0300

    Create start_bts.sh

---
+++ start_bts.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+sysctl -w kernel.sched_rt_runtime_us=-1
+cd /root/configs
+osmo-nitb &
+NITB=$!
+chrt -rr 99 osmo-trx-lms &
+TRX=$!
+sleep 5
+osmo-bts-trx &
+BTS=$!
+wait $NITB $BTS $TRX
